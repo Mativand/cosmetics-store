53a8350a7ff1775cd0f3be896a1300f1416f67a4
mat <ivandaev9@gmail.com>
2024-04-19 18:59:44 -0500

    added fonts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import {FC} from 'react';
 import {Outlet} from "react-router-dom";
+import './styles/App.scss';
 
 export const App: FC = () => {
     return (
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -3,6 +3,7 @@ import {FC} from 'react';
 const Main: FC = () => {
     return (
         <div>
+            <h1>Title</h1>
             main
         </div>
     );
