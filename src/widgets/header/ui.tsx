09a661224b38b3f607e3db57aaac9ebbc00b3cc9
mat <ivandaev9@gmail.com>
2024-04-27 16:43:50 -0500

    header layout

--- src/widgets/header/ui.tsx
+++ src/widgets/header/ui.tsx
@@ -1,23 +1,42 @@
-import {FC} from "react";
+import {FC, useRef} from "react";
 import s from './styles.module.scss';
-import {BsHeart, BsSearch} from "react-icons/bs";
-import {PiHandbagLight} from "react-icons/pi";
-import {CiMenuBurger} from "react-icons/ci";
+import {HiOutlineShoppingBag} from "react-icons/hi";
+import {RiHeart3Line} from "react-icons/ri";
+import {TbSearch} from "react-icons/tb";
 
 export const Header: FC = () => {
+
+    const burgerRef = useRef(null);
+    const listRef = useRef(null);
+
+    const openOrCloseMenu = () => {
+        const burger: HTMLElement = burgerRef.current;
+        burger.classList.toggle(s.active);
+
+        const menu: HTMLElement = listRef.current;
+        menu.classList.toggle(s.active);
+    };
+
     return (
-        <header className={"bg-yellow-500"}>
-            <div className={s.container}>
-                <div className={s.burger}>
-                    <CiMenuBurger/>
-                </div>
-                <div className={s.search}>
-                    <BsSearch size={'18px'}/>
-                </div>
-                <div className={s.logo}>pipette</div>
-                <div className={s.icons}>
-                    <BsHeart size={'18px'}/>
-                    <PiHandbagLight size={'18px'}/>
+        <header className={s.header}>
+            <div className={s.content}>
+                <div className={s.menu}>
+                    <div onClick={openOrCloseMenu} className={s.burger} ref={burgerRef}>
+                        <span></span>
+                    </div>
+                    <div className={s.logo}>pipette</div>
+                    <nav className={s.body}>
+                        <ul className={s.list} ref={listRef}>
+                            <li className={s.link}>О нас</li>
+                            <li className={s.link}>Каталог</li>
+                            <li className={s.link}>Отзывы</li>
+                        </ul>
+                    </nav>
+                    <div className={s.icons}>
+                        <span><TbSearch size={'22px'}/></span>
+                        <span><RiHeart3Line size={'22px'}/></span>
+                        <span><HiOutlineShoppingBag size={'22px'}/></span>
+                    </div>
                 </div>
             </div>
         </header>
